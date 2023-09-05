97e17f7a68a62b8a688b2bf86df9977870e3f162
duong traan <byline2009@gmail.com>
2023-09-05 19:42:26 +0700

    update layout

--- src/AppClass.js
+++ src/AppClass.js
@@ -2,12 +2,12 @@ import React from "react";
 import ComponentLv1 from "./components/ComponentLv1";
 
 class AppClass extends React.Component {
+  state = { name: "State parent" };
   render() {
-    let text = "Duong Tran";
     return (
       <>
         <h1>Hello World</h1>
-        <ComponentLv1 />
+        <ComponentLv1 name={this.state.name} />
       </>
     );
   }
--- src/components/ComponentLv1.jsx
+++ src/components/ComponentLv1.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 
 export default class ComponentLv1 extends Component {
   render() {
+    console.log("props children", this.props);
     return (
       <>
         <h1>Hello, level 1 {this.props.text}</h1>;
